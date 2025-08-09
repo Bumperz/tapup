a4e326750ced280a9ac1452427c70e54d4298210
Bumperz <jake@gigacore.co>
2025-08-08 21:30:19 -0700

    Update:
    - improved functionality

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tapup-v2';
+const CACHE_NAME = 'tapup-v3';
 const ASSETS = [
   './',
   './index.html',
@@ -15,9 +15,14 @@ const ASSETS = [
 ];
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
 });
 self.addEventListener('activate', (event) => {
-  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))));
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
 });
 self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request))); });
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
